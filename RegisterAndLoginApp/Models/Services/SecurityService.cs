4fa32b6f646544c6783c3529e1ac26a2cbdcaa9d
iniekan <iniakpan.e@gmail.com>
2021-03-29 18:40:10 -0600

    DataBase Connection added for authentication and login access

--- RegisterAndLoginApp/Models/Services/SecurityService.cs
+++ RegisterAndLoginApp/Models/Services/SecurityService.cs
@@ -7,20 +7,13 @@ namespace RegisterAndLoginApp.Models.Services
 {
     public class SecurityService
     {
-        List<UserModel> knownUsers = new List<UserModel>();
+        UsersDAO usersDAO = new UsersDAO();
 
-        public SecurityService()
-        {
-            knownUsers.Add(new UserModel { Id = 0, UserName = "rene", Password = "dove" });
-            knownUsers.Add(new UserModel { Id = 1, UserName = "laila", Password = "real" });
-            knownUsers.Add(new UserModel { Id = 2, UserName = "flora", Password = "calg"});
-            knownUsers.Add(new UserModel { Id = 3, UserName = "biggy", Password = "reps" });
-        }
 
         public bool IsValid(UserModel user)
         {
-            // return true with C# keyword ANY()
-            return knownUsers.Any(x => x.UserName == user.UserName && x.Password == user.Password);
+            return usersDAO.findUserByNameAndPassword(user);
+
         }
     }
 }
---
+++ RegisterAndLoginApp/Models/Services/UsersDAO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterAndLoginApp.Models.Services
+{
+    public class UsersDAO
+    {
+        string conntectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test1;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public bool findUserByNameAndPassword(UserModel user)
+        {
+            bool success = false;
+
+            string sqlStatement = "SELECT * FROM dbo.Users WHERE username = @username AND password = @password";
+
+            using (SqlConnection connection = new SqlConnection(conntectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+
+                command.Parameters.Add("@username", System.Data.SqlDbType.VarChar, 50).Value = user.UserName;
+                command.Parameters.Add("@password", System.Data.SqlDbType.VarChar, 50).Value = user.Password;
+
+                try 
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        success = true;
+                    }
+                } 
+                catch(Exception e) 
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+
+            // return true if found in db.user
+            return success;
+        }
+    }
+}
